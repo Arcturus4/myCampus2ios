e6f00d3abc87152c9518f441692f485c3f053044
wilhelmo360 <32298091+wilhelmo360@users.noreply.github.com>
2020-04-28 21:28:39 +0300

    Getting started

---
+++ myCampus2ios/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  myCampus2ios
+//
+//  Created by iosdev on 28/04/2020.
+//  Copyright © 2020 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
