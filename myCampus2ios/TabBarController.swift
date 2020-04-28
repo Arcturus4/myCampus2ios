6f4eb815803260be55ffedfca584d5a3be7103e2
wilhelmo360 <32298091+wilhelmo360@users.noreply.github.com>
2020-04-28 22:22:49 +0300

    skeleton

---
+++ myCampus2ios/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  myCampus2ios
+//
+//  Created by Nea Virtanen on 28.4.2020.
+//  Copyright © 2020 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
