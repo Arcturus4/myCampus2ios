4ec9e69c41c2371e95a6b40d9a9abf36b5f71b1c
svidhaji <svidhaji@gmail.com>
2020-04-29 17:59:01 +0300

    ..

--- myCampus2ios/ViewController.swift
+++ myCampus2ios/ViewController.swift
@@ -10,10 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-<<<<<<< HEAD
-=======
-    
->>>>>>> 00523bc296f52967a9a6ebe721d4cbb6e3a8cd20
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
