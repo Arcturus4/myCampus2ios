43bb0d3f50966777de21c503d8f9d3f3a9c56d1b
svidhaji <svidhaji@gmail.com>
2020-05-01 22:20:41 +0300

    validations added..still working on login validation though

--- myCampus2ios/LoginViewController.swift
+++ myCampus2ios/LoginViewController.swift
@@ -12,10 +12,15 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginEmailText: UITextField!
     @IBOutlet weak var loginPassText: UITextField!
+    @IBOutlet weak var validText: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    var accessToken = (UIApplication.shared.delegate as! AppDelegate).token
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        validText.isHidden = true
         // Do any additional setup after loading the view.
     }
     
@@ -24,16 +29,15 @@ class LoginViewController: UIViewController {
         
         let emailtxt = loginEmailText.text
         let passtxt = loginPassText.text
-        
         let body = User(email: emailtxt!, password: passtxt!)
         let post = APIClient(endp: "/auth/login")
         
         post.networkRequest(body, completion: {result in
             switch result {
             case .success(let body):
-              /*  DispatchQueue.main.async {
-                    print(bodyResp.token)
-                }*/
+               DispatchQueue.main.async {
+                print(self.accessToken)
+                }
                 print("Email is: \(String(describing: body.email))")
                 print("Password is: \(String(describing: body.password))")
             case .failure(let error):
@@ -41,6 +45,39 @@ class LoginViewController: UIViewController {
             }
         })
         
+        guard let emailfield = loginEmailText.text, loginEmailText.text?.count != 0
+            else {
+                validText.isHidden = false
+                validText.text = "Please enter your e-mail.."
+                return
+        }
+        
+        if isValid(emailString: emailfield) == false {
+            validText.isHidden = false
+            validText.text = "Please enter valid e-mail.."
+            self.loginButton.isEnabled = false
+        } else {
+            self.loginButton.isEnabled = true
+        }
+        
+        guard let passwordfield = loginPassText.text, loginPassText.text?.count != 0
+                   else {
+                       validText.isHidden = false
+                       validText.text = "Please enter your password.."
+                       return
+               }
+        if passwordfield.isEmpty == false{
+            self.loginButton.isEnabled = true
+        } else {
+            self.loginButton.isEnabled = false
+        }
+        
+    }
+    
+    func isValid(emailString: String) -> Bool {
+        let eRgx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-za-z]{2,}"
+        let rgxValue = NSPredicate(format: "SELF MATCHES %@", eRgx)
+        return rgxValue.evaluate(with: emailString)
     }
     
 
