725e1fcd6801db8b00279a53d0315147f53a81fc
wilhelmo360 <32298091+wilhelmo360@users.noreply.github.com>
2020-05-04 00:48:23 +0300

    Add Cocoapods and functioning parking

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'myCampus2ios' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for myCampus2ios
+pod "MBCircularProgressBar"
+
+  target 'myCampus2iosTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'myCampus2iosUITests' do
+    # Pods for testing
+  end
+
+end
--- myCampus2ios/ParkingViewController.swift
+++ myCampus2ios/ParkingViewController.swift
@@ -7,105 +7,51 @@
 //
 
 import UIKit
+import MBCircularProgressBar
 
 class ParkingViewController: UIViewController {
     
-    @IBOutlet weak var ParkingP10Top: UIView!
-    @IBOutlet weak var ParkingP10Inside: UIView!
+    @IBOutlet weak var ParkingStackView: UIStackView!
+    
     @IBOutlet weak var ParkingP5: UIView!
+    @IBOutlet weak var ParkinP5Progress: MBCircularProgressBarView!
     
-    @IBOutlet weak var P10TLabel: UILabel!
-    @IBOutlet weak var P10InsLabel: UILabel!
-    @IBOutlet weak var P5Label: UILabel!
+    @IBOutlet weak var ParkingP10Top: UIView!
+    @IBOutlet weak var ParkingP10TopProgress: MBCircularProgressBarView!
     
-    var p10TopUsage = 0.92
-    var p10InsideUsage = 0.34
-    var p5usage = 0.73
+    @IBOutlet weak var ParkingP10Inside: UIView!
+    @IBOutlet weak var ParkingP10InsideProgress: MBCircularProgressBarView!
+    
+    @IBOutlet weak var ParkingElectric: UIView!
+    @IBOutlet weak var ParkingElectricProgress: MBCircularProgressBarView!
+    
+    //Progress Values
+    var P5Progress = 13
+    var p10TopProgress = 24
+    var P10InsideProgress = 45
+    var ElectricProgress = 67
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Parking P10 Percentages
         
-        let circularPath = UIBezierPath(arcCenter: ParkingP10Top.center, radius: 50, startAngle: -.pi  / 2, endAngle: 3 * .pi / 2, clockwise: true)
-            
-        let shapeLayerP10Top = CAShapeLayer()
-        shapeLayerP10Top.path = circularPath.cgPath
-        shapeLayerP10Top.strokeColor = UIColor.white.cgColor
-        shapeLayerP10Top.lineWidth = 5
-        shapeLayerP10Top.strokeEnd = 0
-        shapeLayerP10Top.lineCap = .round
-        shapeLayerP10Top.fillColor = UIColor.clear.cgColor
-        
-        view.layer.addSublayer(shapeLayerP10Top)
-        
-        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        
-        P10TLabel.text = "\(Double(p10TopUsage))%"
-        P10TLabel.textAlignment = .center
-        basicAnimation.toValue = p10TopUsage
-        basicAnimation.duration = 2
-        
-        basicAnimation.fillMode = .forwards
-        basicAnimation.isRemovedOnCompletion = false
-        
-        shapeLayerP10Top.add(basicAnimation, forKey: "parking1oTop")
-        
-        
-        // Parking P10 Inside Percentage
-        
-        let p10InsideCircularPath = UIBezierPath(arcCenter: ParkingP10Inside.center, radius: 50, startAngle: -.pi  / 2, endAngle: 3 * .pi / 2, clockwise: true)
-                
-        let shapeLayerP10Inside = CAShapeLayer()
-        shapeLayerP10Inside.path = p10InsideCircularPath.cgPath
-        shapeLayerP10Inside.strokeColor = UIColor.white.cgColor
-        shapeLayerP10Inside.lineWidth = 5
-        shapeLayerP10Inside.strokeEnd = 0
-        shapeLayerP10Inside.lineCap = .round
-        shapeLayerP10Inside.fillColor = UIColor.clear.cgColor
-        
-        view.layer.addSublayer(shapeLayerP10Inside)
-        
-        let p10InsideBasicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        
-        P10InsLabel.text = "\(Double(p10InsideUsage))%"
-        P10InsLabel.textAlignment = .center
-        p10InsideBasicAnimation.toValue = p10InsideUsage
-        p10InsideBasicAnimation.duration = 2
-        
-        p10InsideBasicAnimation.fillMode = .forwards
-        p10InsideBasicAnimation.isRemovedOnCompletion = false
-        
-        shapeLayerP10Inside.add(p10InsideBasicAnimation, forKey: "parking10Inside")
-        
-        
-        // Parking P5 Animation
-        
-              let p5CircularPath = UIBezierPath(arcCenter: ParkingP5.center, radius: 50, startAngle: -.pi  / 2, endAngle: 3 * .pi / 2, clockwise: true)
-                      
-              let shapeLayerP5 = CAShapeLayer()
-              shapeLayerP5.path = p5CircularPath.cgPath
-              shapeLayerP5.strokeColor = UIColor.white.cgColor
-              shapeLayerP5.lineWidth = 5
-              shapeLayerP5.strokeEnd = 0
-              shapeLayerP5.lineCap = .round
-              shapeLayerP5.fillColor = UIColor.clear.cgColor
-              
-              view.layer.addSublayer(shapeLayerP5)
-              
-              let p5BasicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        
-        
-              P5Label.text = "\(Double(p5usage))%"
-              P5Label.textAlignment = .center
-              p5BasicAnimation.toValue = p5usage
-              p5BasicAnimation.duration = 2
-              
-              p5BasicAnimation.fillMode = .forwards
-              p5BasicAnimation.isRemovedOnCompletion = false
-              
-              shapeLayerP5.add(p5BasicAnimation, forKey: "parking5")
+        //Initialize Animations
+        self.ParkinP5Progress.value = 0
+        self.ParkingP10TopProgress.value = 0
+        self.ParkingP10InsideProgress.value = 0
+        self.ParkingElectricProgress.value = 0
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        
+        //Animate Progresses
+        UIView.animate(withDuration: 5) {
+            self.ParkinP5Progress.value = CGFloat(self.P5Progress)
+            self.ParkingP10TopProgress.value = CGFloat(self.p10TopProgress)
+            self.ParkingP10InsideProgress.value = CGFloat(self.P10InsideProgress)
+            self.ParkingElectricProgress.value = CGFloat(self.ElectricProgress)
+        }
+    }
 }
